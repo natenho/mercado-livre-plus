b534a1ecd4f149687c009baef4241d76518b851c
Renato Lima <natenho@gmail.com>
2017-04-11 02:13:46 -0300

    Cálculo do valor total e inclusão do prazo

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-$(".results-item.article").each(function() {
+$(".results-item").each(function() {
 	
 	var link = $(this).find("h2.list-view-item-title>a").first();
-	var price = $(this).find(".price-wrap").first();
-	 
+	var findResult = $(this).find(".price-wrap .ch-price");
+	
+	if (findResult.length == 0)
+	{
+		findResult = $(this).find(".price-info-cost .ch-price");
+	}
+	
+	var price = findResult.first();
+		 
 	 $.get(link.attr('href'), function(response) {
 		
-		var shipping = $(response).find(".shipping-method-title .ch-price").parent().html();
+		var shipping = $(response).find(".shipping-method-title .ch-price").html();
 		
+		var shippingEstimated = $(response).find(".shipping-estimated").html();
+						
 		if (typeof shipping  != 'undefined')
-			price.append(shipping);
+		{
+			price.append(' + ' + shipping);
+											
+			var total = price.text().replace(/R\$/g, '');
+			total = '(' + total + ') / 100';
+			total = eval(total).toFixed(2);
+			total = total.replace('.', '<sup>');
+			total = total + '</sup>';
+			 
+			price.append(' = ' + total);
+			price.parent().append('<br/>' + shippingEstimated);
+		}
 		
 	 });
 });
