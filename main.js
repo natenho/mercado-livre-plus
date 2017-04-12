9553c958c01633f268816ff74118e3b040ca2b6a
natenho <natenho@gmail.com>
2017-04-12 16:33:18 -0300

    FormataÃÃo de milhares

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+function formatNumber(number)
+{
+	decimalSeparator = ',';
+	thousandSeparator = '.';
+		
+	number += '';
+	integerAndDecimalArray = number.split('.');
+	
+	integerPart = integerAndDecimalArray[0];
+	decimalPart = integerAndDecimalArray.length > 1 ? decimalSeparator + integerAndDecimalArray[1] : '';
+	
+	var thousandGroupRegex = /(\d+)(\d{3})/;
+	
+	while (thousandGroupRegex.test(integerPart)) {
+		integerPart = integerPart.replace(thousandGroupRegex, '$1' + thousandSeparator + '$2');
+	}
+	
+	return integerPart + decimalPart;
+}
+
 $(".results-item").each(function() {
 	
 	var link = $(this).find("h2.list-view-item-title>a").first();
@@ -13,20 +33,22 @@ $(".results-item").each(function() {
 	 $.get(link.attr('href'), function(response) {
 		
 		var shipping = $(response).find(".shipping-method-title .ch-price").html();
-		
-		var shippingEstimated = $(response).find(".shipping-estimated").html();
-						
+										
 		if (typeof shipping  != 'undefined')
 		{
 			price.append(' + ' + shipping);
 											
 			var total = price.text().replace(/R\$/g, '');
+			total = total.replace('.', '');
 			total = '(' + total + ') / 100';
 			total = eval(total).toFixed(2);
 			total = total.replace('.', '<sup>');
 			total = total + '</sup>';
-			 
+			total = formatNumber(total);
+			
 			price.append(' = ' + total);
+			
+			var shippingEstimated = $(response).find(".shipping-estimated").html();
 			price.parent().append('<br/>' + shippingEstimated);
 		}
 		
