6864e23bbf8a945dde81d3e5a692afdc97ea55b8
Renato Lima <natenho@gmail.com>
2020-08-28 01:25:16 -0300

    Corrige o comportamento após atualização do site

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 function extractValueFromPriceText(priceText) {
-    var parts = priceText.trim().split(' ');
-
-    if (parts.length == 2) {
-        return eval(parts[1]);
-    }
-
-    return eval(parts[1] + '.' + parts[2]);
+    return eval(
+        priceText
+        .replace('R$', '')
+        .replace(',', '.'));
 }
 
 function extractValueFromShippingPrice(htmlElement) {
@@ -15,15 +12,15 @@ function extractValueFromShippingPrice(htmlElement) {
     return extractValueFromPriceText(priceText);
 }
 
-$(".results-item").each(function () {
+$(".ui-search-layout__item").each(function () {
 
     var link = $(this).find("a").first();
 
-    var mainTitleDiv = $(this).find(".main-title").first();
+    var mainTitleDiv = $(this).find(".ui-search-item__title").first();
     var findResult = $(this).find(".item__price");
 
     if (findResult.length == 0) {
-        findResult = $(this).find(".price-fraction");
+        findResult = $(this).find(".price-tag");
     }
 
     var productPriceElement = findResult.first();
@@ -32,7 +29,7 @@ $(".results-item").each(function () {
         { contentScriptQuery: "fetchShipping", url: link.attr('href') },
         response => {
 
-            var shippingPriceElement = $(response).find(".shipping-method-title .ch-price");
+            var shippingPriceElement = $(response).find(".shipping-method-title .ch-price").first();
 
             if (typeof shippingPriceElement.html() != 'undefined') {
                 var productPrice = extractValueFromPriceText(productPriceElement.text());
@@ -41,13 +38,14 @@ $(".results-item").each(function () {
                 var totalPrice = (productPrice + shippingPrice).toFixed(2);
 
                 var totalPriceHtml = '<span class="price__fraction">' + totalPrice;
-                totalPriceHtml = totalPriceHtml.replace('.', '</span><span class="price__decimals" style="left:0">');
+                totalPriceHtml = totalPriceHtml.replace('.', '</span><span class="price-tag-cents" style="left:0">');
                 totalPriceHtml = totalPriceHtml + '</span>';
 
-                var shippingPriceHtml = shippingPriceElement.html().replace('<sup>', '<span class="price__decimals" style="left:0">');
+                var shippingPriceHtml = shippingPriceElement.html().replace('<sup>', '<span class="price-tag-cents" style="left:0">');
                 shippingPriceHtml = shippingPriceHtml.replace('</sup>', '</span>');
+                shippingPriceHtml = shippingPriceHtml.replace(' ', '&nbsp;');
 
-                productPriceElement.append(' + ' + shippingPriceHtml + ' = ' + totalPriceHtml);
+                productPriceElement.append('+' + shippingPriceHtml + '= ' + totalPriceHtml);
             }
 
             var shippingDetailElement = $(response).find(".shipping-method-title").parent().parent();
@@ -58,6 +56,14 @@ $(".results-item").each(function () {
                 shippingEstimated = shippingEstimated.replace('\.', '');
 
                 mainTitleDiv.parent().append('<span style="font-size: 14px; font-weight:bold; color: #00a650;">' + shippingEstimated + '</span>');
+            }
+            else
+            {
+                var shippingEstimated = shippingDetailElement.find("span.green").html();
+
+                if (typeof shippingEstimated != 'undefined') {    
+                    mainTitleDiv.parent().append('<span style="font-size: 14px; font-weight:bold; color: #00a650;">' + shippingEstimated + '</span>');
+                }
             }
         }
     );
